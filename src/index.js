5e239054b12a41cbda011677ba21627f2b2c0d17
Amine-Frikha <aminfrikha@outlook.com>
2021-11-09 15:46:25 +0100

    Added Appbar Component

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ButtonAppBar/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
