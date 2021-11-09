f36c560f8d219fc9a4d108cd9e7e2d46f2f23fa8
Amine-Frikha <aminfrikha@outlook.com>
2021-11-09 15:54:38 +0100

    Created URL Submission form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super()
+  }
+  
+  render(){
+    return( 
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={} style={{ display: 'flex',flexDirection: 'column'}}>
+            <TextField value={} onChange={}
+            style={{ marginBottom: 22 }} className="textfield" id="outlined-basic" 
+            label="Please Input Your Link" variant="outlined" />
+            <Button style={{
+            marginBottom: 22,
+            backgroundColor: "#47597E",
+            padding: "10px 20px",
+            fontSize: "18px"}} 
+            variant="contained" type= 'Submit'>Shorten</Button>
+          </form>
+          Your Shortened URL : 
+          <Box component="span" sx={{ p: 2, border: '0.5px dashed grey' }}>
+            <a href={}> {} </a>
+          </Box>
+        </header>
+      </div>)
+   
+  }
 }
+
 
 export default App;
